982bf1b537428517ee7b40e40f5accb4841f5c97
Greg Lind <gwlind@gmail.com>
2013-12-02 16:48:26 -0800

    adding base templates

--- FRDS/silo/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from silo.models import Silo,DataField,DataValue,ValueStore,ValueType
-
-admin.site.register(Silo)
-admin.site.register(DataField)
-admin.site.register(DataValue)
-admin.site.register(ValueStore)
-admin.site.register(ValueType)
