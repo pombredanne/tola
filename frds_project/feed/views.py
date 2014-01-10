18633f3d1502802ba43131dc3b82f2a4855b729c
Greg Lind <gwlind@gmail.com>
2014-01-10 13:49:53 -0800

    export to json in feed

--- frds_project/feed/views.py
+++ frds_project/feed/views.py
@@ -8,7 +8,9 @@ from silo.models import Silo, DataField, ValueStore, ValueType, Read, Feed
 from feed.models import FeedType
 from django.shortcuts import render_to_response
 import dicttoxml,json
+import unicodedata
 from django.core import serializers
+from django.utils import simplejson
 
 
 
@@ -30,14 +32,17 @@ def createFeed(request):
 
 	getSilo = ValueStore.objects.filter(field__silo__id=request.POST['silo_id'])
 	
-	getFeedType = FeedType.objects.filter(pk = request.POST['feed_type'])
+	formatted_data = siloToDict(getSilo)
+	
+	getFeedType = FeedType.objects.get(pk = request.POST['feed_type'])
 	
 	if getFeedType.description == "XML":
-		xml = serializers.serialize("xml", getSilo)
-		return render(request, 'feed/xml.html', {"xml": "xml"}, content_type="application/xhtml+xml")
+		xmlData = serialize(formatted_data)
+		return render(request, 'feed/xml.html', {"xml": xmlData}, content_type="application/xhtml+xml")
 	elif getFeedType.description == "JSON":
-		jsonData = json.dumps(getSilo)
-		return render(request, 'feed/json.html', {"jsonData": "jsonData"}, content_type="application/json")
+		jsonData = simplejson.dumps(formatted_data)
+		print jsonData
+		return render(request, 'feed/json.html', {"jsonData": jsonData}, content_type="application/json")
 
 #DELETE-FEED 
 def deleteFeed(request,id):
@@ -45,6 +50,41 @@ def deleteFeed(request,id):
 	deleteFeed = Feed.objects.get(pk=id).delete()
 	
 	return render(request, 'feed/delete.html')		
+	
+
+#CREATE NEW DATA DICTIONARY OBJECT 
+def siloToDict(silo):
+	parsed_data = {}
+	for d in silo:
+		parsed_lables = unicodedata.normalize('NFKD', d.field.name).encode('ascii','ignore')
+		if d.value_type.value_type == "Char":
+			parsed_values = unicodedata.normalize('NFKD', d.char_store).encode('ascii','ignore')
+		elif d.value_type.value_type == "Int":
+			parsed_values = unicodedata.normalize('NFKD', d.int_store).encode('ascii','ignore')
+		elif d.value_type.value_type == "Bool":
+			parsed_values = unicodedata.normalize('NFKD', d.bool_store).encode('ascii','ignore')
+	
+		parsed_data[parsed_lables] = parsed_values
+	
+	print parsed_data
+	
+	return parsed_data	
+
+#XML for non Model Object Serialization
+def serialize(root):
+        xml = ''
+        for key in root.keys():
+            if isinstance(root[key], dict):
+                xml = '%s<%s>\n%s</%s>\n' % (xml, key, serialize(root[key]), key)
+            elif isinstance(root[key], list):
+                xml = '%s<%s>' % (xml, key)
+                for item in root[key]:
+                    xml = '%s%s' % (xml, serialize(item))
+                xml = '%s</%s>' % (xml, key)
+            else:
+                value = root[key]
+                xml = '%s<%s>%s</%s>\n' % (xml, key, value, key)
+        return xml
 	
 	
 	
