2259becc8ee1739f1d920a0b1b6d63cf696ca8dd
Greg Lind <gwlind@gmail.com>
2014-02-10 16:56:55 -0800

    fix edit buttons and start on modal pop-ups for edits

--- frds_project/display/tables.py
+++ frds_project/display/tables.py
@@ -2,12 +2,12 @@ import django_tables2 as tables
 from silo.models import ValueStore
 
 TEMPLATE = '''
-   <a href="/value_edit/{{ record.id }}" class="tbl_icon edit">Edit</a>
-   <a href="/value_delete/{{ record.id }}" class="tbl_icon delete">Delete</a>
+   <a class="btn btn-default btn-xs" role="button" href="/value_edit/{{ record.id }}">Edit</a>
+   <a class="btn btn-danger btn-xs" role="button" href="/value_delete/{{ record.id }}">Delete</button>
 '''
 
 TEMPLATE2 = '''
-   <a href="/field_edit/{{ record.field.id }}" class="tbl_icon edit">{{record.field}}</a>
+<a data-toggle="modal" href="/field_edit/{{ record.field.id }}" data-target="#columnModal">{{record.field}}</a>
 '''
 
 class SiloTable(tables.Table):
