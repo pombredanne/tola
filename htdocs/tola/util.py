b7f21c7e6471fafe8b3fc66fd85dfbffff5cc634
Greg Lind <gwlind@gmail.com>
2014-11-10 16:58:32 -0800

    fixed CSV download

--- htdocs/tola/util.py
+++ htdocs/tola/util.py
@@ -7,6 +7,7 @@ def siloToDict(silo):
     for d in silo:
         label = unicodedata.normalize('NFKD', d.field.name).encode('ascii','ignore')
         value = unicodedata.normalize('NFKD', d.char_store).encode('ascii','ignore')
+        row = unicodedata.normalize('NFKD', d.row_number).encode('ascii','ignore')
         parsed_data[key_value] = {label : value}
 
         key_value += 1
